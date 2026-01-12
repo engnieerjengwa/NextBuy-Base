e1c310e27fc19018db00249d311b8a665964747d
Phillip Jengwa <PhillipJ@vertice.software>
2026-01-12 15:35:47 +0200

    Add custom form validators and integrate into checkout form
    Introduced validators for whitespace, Luhn algorithm, and card type detection. Updated the checkout form to use these validators, ensuring improved input validation for customer, address, and card details. Includes a unit test for `CustomValidators`.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CartService } from '../../services/cart.service';
 import { CardExpiryService } from '../../services/card-expiry.service';
 import { CartItem } from '../../common/cart-item';
 import { CurrencyPipe, NgFor, NgIf, NgClass } from '@angular/common';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
+import { CustomValidators } from '../../validators/custom-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -45,11 +52,13 @@ export class CheckoutComponent implements OnInit {
     const expirySelector = document.querySelector('.expiry-selector-popup');
     const expiryDisplay = document.querySelector('.expiry-display');
 
-    if (this.showExpirySelector &&
-        expirySelector &&
-        expiryDisplay &&
-        !expirySelector.contains(target) &&
-        !expiryDisplay.contains(target)) {
+    if (
+      this.showExpirySelector &&
+      expirySelector &&
+      expiryDisplay &&
+      !expirySelector.contains(target) &&
+      !expiryDisplay.contains(target)
+    ) {
       this.showExpirySelector = false;
     }
   }
@@ -63,29 +72,81 @@ export class CheckoutComponent implements OnInit {
   ngOnInit() {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        lastName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ]),
       }),
+
+      // Shipping address is required, so it's set to required too'
       shippingAddress: this.formBuilder.group({
-        country: [''],
-        state: [''],
-        city: [''],
-        street: [''],
-        zipCode: [''],
+        country: new FormControl('', [Validators.required]),
+        state: new FormControl('', [Validators.required]),
+        city: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        street: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        zipCode: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
       }),
+
+      // Billing address is optional, so it's set to optional too'
       billingAddress: this.formBuilder.group({
-        country: [''],
-        state: [''],
-        city: [''],
-        street: [''],
-        zipCode: [''],
+        country: new FormControl('', [Validators.required]),
+        state: new FormControl('', [Validators.required]),
+        city: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        street: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        zipCode: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
       }),
+
+      // Credit card details are optional, so they're set to optional too'
       cardDetails: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('', [Validators.required]),
+        nameOnCard: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          CustomValidators.whiteSpaceValidator,
+        ]),
+        cardNumber: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[0-9]{16}'),
+          CustomValidators.luhnValidator,
+        ]),
+        securityCode: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[0-9]{3}'),
+        ]),
         cardExpires: ['', [Validators.required]],
         expiryMonth: ['', [Validators.required]],
         expiryYear: ['', [Validators.required]],
@@ -98,20 +159,16 @@ export class CheckoutComponent implements OnInit {
     // Subscribe to shipping address country changes
     this.checkoutFormGroup
       .get('shippingAddress.country')
-      ?.valueChanges.subscribe(
-        country => {
-          this.getStates(country.code, this.shippingAddressStates);
-        }
-      );
+      ?.valueChanges.subscribe((country) => {
+        this.getStates(country.code, this.shippingAddressStates);
+      });
 
     // Subscribe to billing address country changes
     this.checkoutFormGroup
       .get('billingAddress.country')
-      ?.valueChanges.subscribe(
-        country => {
-          this.getStates(country.code, this.billingAddressStates);
-        }
-      );
+      ?.valueChanges.subscribe((country) => {
+        this.getStates(country.code, this.billingAddressStates);
+      });
 
     // Subscribe to shipping address changes
     this.checkoutFormGroup
@@ -123,6 +180,13 @@ export class CheckoutComponent implements OnInit {
         }
       });
 
+    // Subscribe to card number changes to detect card type
+    this.checkoutFormGroup
+      .get('cardDetails.cardNumber')
+      ?.valueChanges.subscribe((cardNumber) => {
+        this.detectAndSetCardType(cardNumber);
+      });
+
     // Get cart items and totals
     this.cartItems = this.cartService.cartItems;
     this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
@@ -132,17 +196,80 @@ export class CheckoutComponent implements OnInit {
     this.cartService.computeCartTotals();
 
     // Populate countries
-    this.cardExpiryService.getCountries().subscribe(
-      data => {
-        this.countries = data;
-        console.log('Retrieved countries: ' + JSON.stringify(data));
-      }
-    );
+    this.cardExpiryService.getCountries().subscribe((data) => {
+      this.countries = data;
+      console.log('Retrieved countries: ' + JSON.stringify(data));
+    });
   }
 
   onSubmit() {
     console.log('Handling the submit button');
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
     console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  // Customer getters
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName')!;
+  }
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName')!;
+  }
+  get email() {
+    return this.checkoutFormGroup.get('customer.email')!;
+  }
+
+  // Shipping Address getters
+  get shippingAddressStreet() {
+    return this.checkoutFormGroup.get('shippingAddress.street')!;
+  }
+  get shippingAddressCity() {
+    return this.checkoutFormGroup.get('shippingAddress.city')!;
+  }
+  get shippingAddressState() {
+    return this.checkoutFormGroup.get('shippingAddress.state')!;
+  }
+  get shippingAddressCountry() {
+    return this.checkoutFormGroup.get('shippingAddress.country')!;
+  }
+  get shippingAddressZipCode() {
+    return this.checkoutFormGroup.get('shippingAddress.zipCode')!;
+  }
+
+  // Billing Address getters
+  get billingAddressStreet() {
+    return this.checkoutFormGroup.get('billingAddress.street')!;
+  }
+  get billingAddressCity() {
+    return this.checkoutFormGroup.get('billingAddress.city')!;
+  }
+  get billingAddressState() {
+    return this.checkoutFormGroup.get('billingAddress.state')!;
+  }
+  get billingAddressCountry() {
+    return this.checkoutFormGroup.get('billingAddress.country')!;
+  }
+  get billingAddressZipCode() {
+    return this.checkoutFormGroup.get('billingAddress.zipCode')!;
+  }
+
+  // Card Details getters
+  get cardType() {
+    return this.checkoutFormGroup.get('cardDetails.cardType')!;
+  }
+  get nameOnCard() {
+    return this.checkoutFormGroup.get('cardDetails.nameOnCard')!;
+  }
+  get cardNumber() {
+    return this.checkoutFormGroup.get('cardDetails.cardNumber')!;
+  }
+  get securityCode() {
+    return this.checkoutFormGroup.get('cardDetails.securityCode')!;
+  }
+  get cardExpires() {
+    return this.checkoutFormGroup.get('cardDetails.cardExpires')!;
   }
 
   copyShippingToBilling(event: any) {
@@ -168,6 +295,18 @@ export class CheckoutComponent implements OnInit {
     return cartItem.quantity * cartItem.unitPrice;
   }
 
+  incrementQuantity(cartItem: CartItem) {
+    this.cartService.incrementQuantity(cartItem);
+  }
+
+  decrementQuantity(cartItem: CartItem) {
+    this.cartService.decrementQuantity(cartItem);
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem);
+  }
+
   // Set up card expiry data (months and years)
   setupCardExpiryData() {
     const currentYear: number = this.cardExpiryService.getThisYear();
@@ -177,17 +316,15 @@ export class CheckoutComponent implements OnInit {
     this.selectedYear = currentYear;
     this.selectedMonth = currentMonth;
 
-    this.cardExpiryService.getCardYear().subscribe(
-      (data: number[]) => {
-        this.cardYears = data;
-      }
-    );
+    this.cardExpiryService.getCardYear().subscribe((data: number[]) => {
+      this.cardYears = data;
+    });
 
-    this.cardExpiryService.getValidMonths(currentYear).subscribe(
-      (data: number[]) => {
+    this.cardExpiryService
+      .getValidMonths(currentYear)
+      .subscribe((data: number[]) => {
         this.cardMonths = data;
-      }
-    );
+      });
   }
 
   toggleExpirySelector() {
@@ -211,7 +348,9 @@ export class CheckoutComponent implements OnInit {
       this.selectedMonth = month;
       this.selectedYear = this.displayYear;
       this.checkoutFormGroup.get('cardDetails.expiryMonth')?.setValue(month);
-      this.checkoutFormGroup.get('cardDetails.expiryYear')?.setValue(this.displayYear);
+      this.checkoutFormGroup
+        .get('cardDetails.expiryYear')
+        ?.setValue(this.displayYear);
       this.updateCardExpiresValue();
       this.showExpirySelector = false;
     }
@@ -231,15 +370,26 @@ export class CheckoutComponent implements OnInit {
 
   // Check if a month is selected
   isMonthSelected(month: number): boolean {
-    return this.selectedMonth === month && this.selectedYear === this.displayYear;
+    return (
+      this.selectedMonth === month && this.selectedYear === this.displayYear
+    );
   }
 
   // Get the month name from the month number
   getMonthName(month: number): string {
     const monthNames = [
-      'Jan', 'Feb', 'Mar', 'Apr',
-      'May', 'Jun', 'Jul', 'Aug',
-      'Sep', 'Oct', 'Nov', 'Dec'
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
     ];
     return monthNames[month - 1];
   }
@@ -250,27 +400,66 @@ export class CheckoutComponent implements OnInit {
     const year = +this.checkoutFormGroup.get('cardDetails.expiryYear')?.value;
 
     if (month && year) {
-      const formattedDate: string = this.cardExpiryService.formatExpiryDate(month, year);
-      this.checkoutFormGroup.get('cardDetails.cardExpires')?.setValue(formattedDate);
+      const formattedDate: string = this.cardExpiryService.formatExpiryDate(
+        month,
+        year
+      );
+      this.checkoutFormGroup
+        .get('cardDetails.cardExpires')
+        ?.setValue(formattedDate);
       this.formattedExpiryDate = formattedDate;
     }
   }
 
-  /**
-   * Get states/provinces for the given country code
-   * @param countryCode The country code to get states for
-   * @param stateArray The array to populate with states
-   */
   getStates(countryCode: string, stateArray: State[]) {
     this.cardExpiryService.getProvinces(countryCode).subscribe(
-      data => {
-        stateArray.length = 0; // Clear the array
-        data.forEach(state => stateArray.push(state));
-        console.log(`Retrieved states for ${countryCode}: ${JSON.stringify(data)}`);
+      (data) => {
+        stateArray.length = 0;
+        data.forEach((state) => stateArray.push(state));
+        console.log(
+          `Retrieved states for ${countryCode}: ${JSON.stringify(data)}`
+        );
       },
-      error => {
+      (error) => {
         console.error(`Error retrieving states for ${countryCode}: ${error}`);
       }
     );
+  }
+
+  /**
+   * Detects the card type based on the card number and sets the card type field
+   * @param cardNumber The credit card number
+   */
+  detectAndSetCardType(cardNumber: string) {
+    if (!cardNumber) {
+      return;
+    }
+
+    // Remove any spaces or non-digit characters
+    const cleanCardNumber = cardNumber.replace(/\D/g, '');
+
+    // Only proceed if we have at least 4 digits (enough to identify card type)
+    if (cleanCardNumber.length < 4) {
+      return;
+    }
+
+    let cardType = 'Unknown';
+
+    // Visa: Starts with 4
+    if (/^4/.test(cleanCardNumber)) {
+      cardType = 'Visa';
+    }
+    // Mastercard: Starts with 51-55 or 2221-2720
+    else if (/^5[1-5]/.test(cleanCardNumber) ||
+             /^(222[1-9]|22[3-9]|2[3-6]\d|27[0-1]\d|2720)/.test(cleanCardNumber)) {
+      cardType = 'MasterCard';
+    }
+    // American Express: Starts with 34 or 37
+    else if (/^3[47]/.test(cleanCardNumber)) {
+      cardType = 'AmericanExpress';
+    }
+
+    // Set the card type in the form
+    this.checkoutFormGroup.get('cardDetails.cardType')?.setValue(cardType);
   }
 }
